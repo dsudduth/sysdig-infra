cbe5d805a5ec5213043389069f90fc76167a5495
Derek Sudduth <suddd@amazon.com>
2023-10-29 14:41:05 -0700

    initial cdk implementation

---
+++ bin/deployment.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { VpcStack } from '../lib/vpc-stack';
+
+const app = new cdk.App();
+const prefix = stackPrefix(app);
+
+// Create the VPC stack
+const vpcStack = new VpcStack(app, `${prefix}VPCStack`);
+
+/**
+ * Get the stack prefix from the context
+ * @param stack The stack to get the prefix for
+ * @returns The stack prefix or 'Sysdig-' if not defined
+ */
+function stackPrefix(stack: Construct): string {
+  const stackPrefix = stack.node.tryGetContext('stackPrefix');
+
+  if (stackPrefix !== undefined) {
+      return stackPrefix.trim();
+  }
+
+  return 'Sysdig-';
+}
---
+++ lib/vpc-stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+export class VpcStack extends Stack {
+  public readonly vpc: Vpc;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    this.vpc = new Vpc(this, 'VPC', {
+      maxAzs: 2,
+    });
+  }
+}
